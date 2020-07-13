c62c0dce9b0e51b735f385a8ba6a6c0683d7197d
Pheladi Mabitsela <phmabits@c5r3s5.wethinkcode.co.za>
2020-07-13 12:13:15 +0200

    ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,4 +1,14 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phmabits <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 09:24:30 by phmabits          #+#    #+#             */
+/*   Updated: 2020/07/13 11:47:34 by phmabits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 void	ft_putchar(char c)
 {
@@ -16,8 +26,3 @@ void 	ft_print_reverse_alphabet(void)
 	}
 }
 
-int main()
-{
-	ft_print_reverse_alphabet();
-	return(0);
-}
